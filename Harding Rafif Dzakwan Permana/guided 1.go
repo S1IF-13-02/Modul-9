d73e179aabd8cce4d70c6a07d65c357e637d3908
Harding Rafif <hardingrdp3@gmail.com>
2025-11-13 11:48:06 +0700

    Add files via upload

---
+++ Harding Rafif Dzakwan Permana/guided 1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main () {
+	var n int 
+	fmt.Print("Masukan nilai n :")
+	fmt.Scan(&n)
+
+	if n < 0 {
+		n = -n	
+	}
+	fmt.Print("Nilai Absolut : ", n)
+}
---
+++ Harding Rafif Dzakwan Permana/guided 2 .go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"         
+
+func main() {         
+    var bilangan int  
+    var teks string   
+
+    fmt.Scan(&bilangan)   
+
+    teks = "bukan positif" 
+
+    if bilangan > 0 {      
+        teks = "positif"   
+    }
+
+    fmt.Println(teks)      
+}
