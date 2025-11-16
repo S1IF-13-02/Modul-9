2dd179f491f5b9c3ee9c461322262137ca3abf45
jimmyharlindo05 <jimmyharlindojiwo@gmail.com>
2025-11-16 18:42:07 +0700

    Add files via upload

---
+++ Jimmy Harlindo/no 3 guided.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main(){
+	var bilangan int
+	var hasil bool
+
+	fmt.Print("masukan bilangan")
+	fmt.Scan(&bilangan)
+
+	hasil = false
+	if bilangan < 0 && bilangan%2 == 0{
+		hasil = true
+	}
+fmt.Println(hasil)
+}
